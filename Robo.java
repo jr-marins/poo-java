e4a9ab6909d2ed26d5563214ff9dabf8b7001c79
Marcos Mr <marcosmarinsjr@gmail.com>
2024-08-18 14:01:59 -0300

    apenas comentario

--- Robo.java
+++ Robo.java
@@ -1,15 +1,6 @@
 /*
-Welcome to JDoodle!
-
-You can execute code here in 88 languages. Right now you’re in the Java IDE.
-
-  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
-
-  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
-
-  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
-
-Want to change languages? Try the search bar up the top.
+Entendedo como funciona a função principal e as demais palavaras
+reservadas de java.
 */
 
 public class Robo {
