76dcbfb4b678eb6c7782bd951347e915e5cccbd0
Marcos Mr <marcosmarinsjr@gmail.com>
2024-08-23 22:21:30 -0300

    Introdução a tipo de dados

---
+++ aula2_formata.java
@@ -0,0 +1,20 @@
+/*
+Usando formatação para apresentação de dados
+
+para int 
+    %d
+float
+    $f
+string
+    %s
+
+ */
+
+publi class Main {
+
+    public static void main(String[] args) {
+        double valor = 23.08;
+
+        System.out.printf("O valor é %f", valor)
+    }
+}
