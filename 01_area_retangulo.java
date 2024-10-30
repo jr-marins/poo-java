08cf11c11a5bb2f11e73b36596c760b6a90cd535
Marcos Mr <marcosmarinsjr@gmail.com>
2024-10-29 21:04:07 -0300

    Calculando area de retangulo

---
+++ 01_area_retangulo.java
@@ -0,0 +1,19 @@
+
+/**
+ *
+ * @author marco
+ */
+public class AreaRetangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double base = 30.20;
+        double altura = 23;
+        double area = altura * base;
+        
+        System.out.printf("um retângulo com %f de altura e %f de base, tem uma area de %f\n", altura, base, area);
+    }
+    
+}
